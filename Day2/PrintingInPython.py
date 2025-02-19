8e4a3a7e2577efdbda239fc9bf0c03d8a23704fc
pigaur <roninwhisperer@gmail.com>
2025-02-19 06:42:47 +0530

    adding data

---
+++ Day2/PrintingInPython.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+print('Hello World!')
+
+print(5 * 9)
+
+print (5 + 5)
+
+print("The End", "is it?", "Keep watching to learn Python")
+
+print("The End\n" + "is it?\n" + "Keep watching to learn Python")
